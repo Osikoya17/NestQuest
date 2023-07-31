781cd3bead3664bd5d18a0b06e940f1cf58e4b13
Osikoya17 <osikoyaolaoluwa2021@gmail.com>
2023-07-31 11:51:21 +0100

    Responsiveness of signup , login and navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,9 @@
+'use client'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Button} from 'antd'
+import Link from "next/link";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,17 +16,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className='flex flex-row w-full h-16 p-5'>
+        <nav className='flex flex-row justify-between w-full  h-16 p-5'>
           <div className="basis-1/4 flex justify-center text-sm"><h1>logo<b>NestQuest</b></h1></div>
-          <div className="basis-1/2 flex justify-evenly text-slate-500">
+          <div className=" hidden basis-1/2 md:flex  justify-evenly text-slate-500">
             <h6 className='text-sm font-bold border-b-2 border-black text-black '>Home</h6>
             <h6 className='text-sm'>Search</h6>
             <h6 className='text-sm'>FAQs</h6>
             <h6 className='text-sm'>Contact Us</h6>
           </div>
           <div className="basis-1/4 flex justify-center">
-            <Button type='text' className='text-sm'>Sign In</Button>
-            <Button type='primary' className='bg-[#0042EC] border-none text-sm ml-2 h-8 text-white'>Sign Up</Button>
+            <Link href={"/login"} ><Button type='text' className='text-sm'>Sign In</Button></Link>
+            <Link href={"/signup"} ><Button type='primary' className='bg-[#0042EC] border-none text-sm ml-2 h-8 text-white'>Sign Up</Button></Link>
           </div>
         </nav>
         {children}
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,33 +1,35 @@
-
 import { Button,Checkbox, Input} from 'antd';
 
 
 export default function signup() {
     return (
-      <main className="flex items-center pt-5  flex-col">
-        <h6 className="">logo</h6>
+      // Main Body
+      <main className="flex items-center pt-5 p-5 lg:p-0  flex-col">
+        {/* Logo */}
+        <h6 className="mt-10 lg:mt-5">logo</h6>
         <h4 className="mt-2 text-xl font-bold text-[#343434]">Create an account </h4>
-        <section className=" mt-5 flex flex-col shadow-lg p-4 w-[35%] border border-slate-100">
+        {/* FORM Section */}
+        <section className="w-full  mt-5 flex flex-col shadow-lg p-4 lg:w-[35%] border border-slate-100">
             <div className='p-2'>
             <label htmlFor="fullname" className='text-sm text-[#353535]'>Fullname</label>
-            <Input className='h-7' name='fullname'/>
+            <Input className='h-8' name='fullname'/>
             </div>
             <div className='p-2 mt-3'>
             <label htmlFor="email" className='text-sm text-[#353535]'>Email address</label>
-            <Input className='h-7' name='email'/>
+            <Input className='h-8' name='email'/>
             </div>
             <div className='p-2 mt-3'>
             <label htmlFor="password" className='text-sm text-[#353535]'>Password</label>
-            <Input className='h-7' name='password' label="Fullname"/>
+            <Input className='h-8' name='password' label="Fullname"/>
             </div>
             <div className='flex justify-between  p-2 mt-3'>
-            <Button className='w-48 h-7 text-[#ABA5A5]'> I am a Student</Button>
-            <Button className='w-48 h-7 text-[#ABA5A5]'>I am a LandLord</Button>
+            <Button className='w-48 h-10 lg:h-7 text-[#ABA5A5]'> I am a Student</Button>
+            <Button className='w-48 h-10 lg:h-7 text-[#ABA5A5]'>I am a LandLord</Button>
             </div>
             <Checkbox className='p-2 text-[#353535] mt-3'>
             I agree to all Terms and Conditions
             </Checkbox>
-            <Button type='primary' className='mt-4 bg-[#0042EC] border-none text-sm ml-2 text-white'>Sign Up</Button>
+            <Button type='primary' className='mt-4 h-10 bg-[#0042EC] border-none text-sm ml-2 text-white'>Sign Up</Button>
         </section>
 
         <h6 className='mt-5 text-sm'> Already have an account? <span className='text-[#0042EC]'>Sign In</span></h6>
